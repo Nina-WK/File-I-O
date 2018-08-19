ce8e015c2acb13b6254ba714c4c8d778a60930a9
Nikolina Weg Krsticevic <nina.weg@gmail.com>
2018-08-19 12:31:14 +0000

    Writing to a file with Python_4

--- write.py
+++ write.py
@@ -1,3 +1,5 @@
-f = open('newfile.txt', 'a') #append - the content will be appended to the end of the file
-f.write("Hello\n") #newline character
+f = open('newfile.txt', 'w')
+lines = ['Hello', 'World', 'Welcome','To', 'File IO']
+text = '\n'.join(lines)
+f.writelines(text)
 f.close()
